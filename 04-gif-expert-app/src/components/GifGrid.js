3e05ccd23c904597f9186e05e19379a5123f86ed
Leonardo Medina <lmedina@grupocontext.com>
2020-06-21 14:18:51 -0500

    fin seccion 6 :: giftExpertApp

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,25 +1,15 @@
 import React from 'react';
-/* import {GifGridItem } from './GifGridItem'; */
+import {GifGridItem } from './GifGridItem';
 import { useFetchGifs } from '../hooks/useFetchGifs';
 /* import { getGifs } from '../helpers/getGifs'; */
 
 
 export const GifGrid = ({ category }) => {
 
-    const { loading } = useFetchGifs();
+    const {data: images,  loading } = useFetchGifs(category);
 
    /*  const [images, setImages] = useState([]) */
 
-    /**
-     * SOLUCION, add in the [ category ]
-     * 
-     * React Hook useEffect has a missing dependency: 'getGifs'. 
-     * Either include it or remove the dependency array  react-hooks/exhaustive-deps
-     */
-  /*   useEffect(() => {
-        getGifs(category).then(setImages ); // img => setImages ( img )
-    }, [ category ] );
- */
   
 
     return (
@@ -27,11 +17,9 @@ export const GifGrid = ({ category }) => {
             <h3>{category}</h3>
             <div className="card-grid">
 
-            {
-                loading ? <h4>C A R G A N D O . . .</h4>: 'Data cargada'
-            }
-             {/*   {  <ol>
-                    {
+            {loading && <h4 >C A R G A N D O . . .</h4>}
+                <ol>
+                   {
                         images.map(img => (
                             <GifGridItem
                                 key={img.id}
@@ -39,7 +27,8 @@ export const GifGrid = ({ category }) => {
                             />
                         ))
                     }
-                </ol> } */}
+                </ol>
+           
             </div>
         </>
     )
--- 04-gif-expert-app/src/hooks/useFetchGifs.js
+++ 04-gif-expert-app/src/hooks/useFetchGifs.js
@@ -1,17 +1,31 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { getGifs } from '../helpers/getGifs';
 
-export const useFetchGifs = () => {
+export const useFetchGifs = (category) => {
     const [state, setState] = useState({
         data: [],
         loading: true
     });
 
-    setTimeout(() => {
-        setState({
-            data: [1, 2, 3, 4, 5],
-            loading: false
-        })
-    }, 3000);
+    /**
+     * useEffect: se utiliza para evitar que se realice una peticion fetch
+     * cada que el estado del componente cambia, ademas
+     * Los efectos NO puedes ser async 
+     * SOLUCION, add in the [ category ]
+     * 
+     * React Hook useEffect has a missing dependency: 'getGifs'. 
+     * Either include it or remove the dependency array  react-hooks/exhaustive-deps
+     */
+    useEffect(() => {
+        getGifs(category).then(imgs => {
+            setState({
+                data: imgs,
+                loading: false
+            })
+        });
+    }, [category]);
+
+
 
     return state;
 }
